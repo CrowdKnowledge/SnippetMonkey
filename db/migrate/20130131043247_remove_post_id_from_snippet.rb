4cd3dba8e6b07e5ec76f2dc683c27fd2c3aa700b
kannancet <krxcet@gmail.com>
2013-02-01 12:50:07 +0530

    REsolving db migration

--- db/migrate/20130131043247_remove_post_id_from_snippet.rb
+++
@@ -1,9 +0,0 @@
-class RemovePostIdFromSnippet < ActiveRecord::Migration
-  def up
-    remove_column :snippets, :post_id
-  end
-
-  def down
-    add_column :snippets, :post_id, :integer
-  end
-end
