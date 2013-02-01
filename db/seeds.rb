94325e79eb50870d5710796f7cd59f0cd1425058
kannancet <krxcet@gmail.com>
2013-02-01 12:08:46 +0530

    Postgres heroku config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Technology.create(name: 'Django', icon_path: 'django.jpg')
+Technology.create(name: 'Python', icon_path: 'python.png')
+Technology.create(name: 'Ruby', icon_path: 'ruby.jpg')
+Technology.create(name: 'Ruby on Rails', icon_path: 'ruby_on_rails.jpg')
