9650723deee0d3080536459886b2ded598dd3a45
kannancet <krxcet@gmail.com>
2013-02-05 11:19:06 +0530

    Commiting all the changes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ SniptMonkey::Application.routes.draw do
   match '/about_us', :to => "snippets#about_us", :via => :get
   match '/privacy_policy', :to => "snippets#privacy_policy", :via => :get
   match '/license', :to => "snippets#license", :via => :get
-  devise_for :users, :controllers => { :sessions => "users/sessions" , :registrations => "users/registrations", :omniauth_callbacks => "users/omniauth_callbacks", :activities => "users/activities"}
+  devise_for :users, :controllers => { :sessions => "users/sessions" , :registrations => "users/registrations", :omniauth_callbacks => "omniauth_callbacks", :activities => "users/activities"}
  devise_scope :user do
    post '/users/upload_avatar' => 'users/activities#upload_avatar'
    post '/users/change_profile_info' => 'users/activities#change_profile_info'
