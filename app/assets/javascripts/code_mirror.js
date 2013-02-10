66d485d23d0f3d69168ae5e3d63f0ab3704c2b4d
kannancet <krxcet@gmail.com>
2013-02-11 01:09:13 +0530

    Release 1.0

---
+++ app/assets/javascripts/code_mirror.js
@@ -0,0 +1,8 @@
+	$(document).ready(function() {
+	    $('#code').each(function() {
+	         var editor = CodeMirror.fromTextArea(this, {
+	             lineNumbers : true,
+	             matchBrackets : true
+	         });
+	     })
+	})
